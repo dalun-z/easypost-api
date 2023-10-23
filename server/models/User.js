d75d5e6da7a9b2b09e5212470bc3457cbc0ca0a6
build a home <dzhang1238@gmail.com>
2023-10-23 16:58:56 -0700

    update user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const { Int32 } = require('mongodb')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
     role: {
@@ -74,6 +76,23 @@ const UserSchema = new mongoose.Schema({
         default: 0,
     },
 })
+
+UserSchema.pre('save', async function() {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({
+        userId: this._id,
+        email: this.emial,
+    }, process.env.JWT_SECRET)
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch;
+}
 
 
 module.exports = mongoose.model('User', UserSchema)
