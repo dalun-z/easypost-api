8450982596ef214bcce06ac52e88d36dc855d612
build a home <dzhang1238@gmail.com>
2023-10-23 17:44:37 -0700

    add middleware

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // connect to DB
 const connectDB = require('./db/connect')
 
+// routers
+const authRouter = require('./routes/auth')
 const shipmentRouter = require('./routes/shipments')
 
-app.use('/api/v1/', shipmentRouter);
+// error handler
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/', shipmentRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+// connection
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
