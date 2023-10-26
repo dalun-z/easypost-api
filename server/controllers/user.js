2aca693bb60a3bcedb5635eec987464779bd06a8
build a home <dzhang1238@gmail.com>
2023-10-26 10:01:41 -0700

    added updateUser and deleteUser API request

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,7 +6,7 @@ const getAllUsers = async (req, res) => {
         const users = await User.find();
         res.status(200).json(users);
     } catch (err) {
-        res.status(500).json({ message: `Error getting users `});
+        res.status(500).json({ message: `Error getting users ` });
     }
 };
 
@@ -24,7 +24,44 @@ const getUserByEmail = async (req, res) => {
     }
 };
 
+const updateUser = async (req, res) => {
+    const userId = req.params._id;
+    const userData = req.body;
+
+    try {
+        // Find the user by their ID and update their information.
+        const updatedUser = await User.findByIdAndUpdate(userId, userData, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json({ message: 'Error updating user' });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const userId = req.params._id;
+
+    try {
+        // Find the user by their ID and delete them.
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error deleting user' });
+    }
+};
+
 module.exports = {
     getAllUsers,
     getUserByEmail,
+    updateUser,
+    deleteUser,
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllUsers, getUserByEmail } = require('../controllers/user')
+const { getAllUsers, getUserByEmail, updateUser, deleteUser } = require('../controllers/user')
 
 router.get('/getallusers', getAllUsers);
 router.get('/getuser/:email', getUserByEmail);
+router.put('/updateuser/:_id', updateUser);
+router.delete('/deleteuser/:_id', deleteUser);
 
 module.exports = router
