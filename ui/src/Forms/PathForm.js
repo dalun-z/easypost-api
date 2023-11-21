0faa69dd26ba0d81896b1b44261574a22b99fb1e
build a home <dzhang1238@gmail.com>
2023-11-21 15:38:13 -0800

    update path form input logic

--- ui/src/Forms/PathForm.js
+++ ui/src/Forms/PathForm.js
@@ -3,63 +3,68 @@ import '../css/PathForm.css';
 
 function MemberForm() {
     const [formData, setFormData] = useState({
-        名称: '',
-        费率: '',
-        'Carrier ID*': '',
-        'Carrier&Service': '',
+        名称: { value: '', required: true },
+        费率: { value: '', required: true },
+        'Carrier ID': { value: '', required: true },
+        'Carrier&Service': { value: '', required: true },
         签名: '',
-        'Api Key*': '',
-        name: '',
-        street1: '',
+        'Api Key': { value: '', required: true },
+        name: { value: '', required: true },
+        street1: { value: '', required: true },
         street2: '',
-        city: '',
-        'state*': '',
-        'zip code*': '',
-        'country*': '',
-        'phone*': '',
+        city: { value: '', required: true },
+        'state': { value: '', required: true },
+        'zip code': { value: '', required: true },
+        'country': { value: '', required: true },
+        'phone': { value: '', required: true },
     });
 
     const handleInputChange = (field, value) => {
-        const updatedFormData = { ...formData, [field]: value };
+        const updatedFormData = { ...formData, [field]: { ...formData[field], value } };
         setFormData(updatedFormData);
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Here you can add your logic to handle form submission
-        // For example, you can send the formData to an API endpoint
+        const isFormValid = Object.values(formData).every(field => !field.required || field.value.trim() !== '');
 
-        // Reset the form fields after submission (you can remove this if not needed)
-        setFormData({
-            名称: '',
-            费率: '',
-            'Carrier ID*': '',
-            'Carrier&Service': '',
-            签名: '',
-            'Api Key*': '',
-            name: '',
-            street1: '',
-            street2: '',
-            city: '',
-            'state*': '',
-            'zip code*': '',
-            'country*': '',
-            'phone*': '',
-        });
+        if (isFormValid) {
+            // setFormData({
+            //     名称: '',
+            //     费率: '',
+            //     'Carrier ID': '',
+            //     'Carrier&Service': '',
+            //     签名: '',
+            //     'Api Key': '',
+            //     name: '',
+            //     street1: '',
+            //     street2: '',
+            //     city: '',
+            //     'state': '',
+            //     'zip code': '',
+            //     'country': '',
+            //     'phone': '',
+            // });
+            alert('Form has been submitted!');
+        } else {
+            alert('Please fill in all required fields');
+        }
+
     };
 
     const renderInputFields = () => (
         <div className="path-selection">
             <h2>Path Information</h2>
             <form onSubmit={handleSubmit} className='path-form'>
-                {Object.entries(formData).map(([field, value]) => (
+                {Object.entries(formData).map(([field, { value, required }]) => (
                     <React.Fragment key={field}>
                         <label>{field.replace(/\*/g, '').replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase())}:</label>
                         <input
                             type="text"
                             value={value}
                             onChange={(e) => handleInputChange(field, e.target.value)}
+                            required={required}
                         />
                     </React.Fragment>
                 ))}
